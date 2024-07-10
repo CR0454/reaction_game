54698ccd739e6b9db794d908ccba5aae86b3fcbe
oezgeb <oezgebayram@gmail.com>
2024-07-10 12:55:13 +0200

    Highscore Function

---
+++ cmake-build-debug/CMakeFiles/reaction_game.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/oezge/Git/reaction_game/src/highscore.cpp" "CMakeFiles/reaction_game.dir/src/highscore.cpp.obj" "gcc" "CMakeFiles/reaction_game.dir/src/highscore.cpp.obj.d"
+  "C:/Users/oezge/Git/reaction_game/src/main.cpp" "CMakeFiles/reaction_game.dir/src/main.cpp.obj" "gcc" "CMakeFiles/reaction_game.dir/src/main.cpp.obj.d"
+  "C:/Users/oezge/Git/reaction_game/src/menu.cpp" "CMakeFiles/reaction_game.dir/src/menu.cpp.obj" "gcc" "CMakeFiles/reaction_game.dir/src/menu.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
